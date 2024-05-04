32b4c17d5b9c7e26790b0d5a4965790e9da94237
ar61 <vectorhashmap@gmail.com>
2024-05-03 22:16:56 -0400

    Create FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Abhinav Rathod on 5/3/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
